59a58146b9a04890afa780030ff0cb7bd35045dc
raffi zidane w <jidan7224@gmail.com>
2024-09-18 20:56:24 +0800

    Create 2409106037_raffi zidane w_A2'24.py

---
+++ pertemuan 3/2409106037_raffi zidane w_A2'24.py
@@ -0,0 +1,55 @@
+# lu belum selesai dan,jgn males kerjain oi lanjutkan liat yt atau baca w3 kayak kata aslab
+# fitur = int(input("pilih fitur : "))
+# match fitur :
+#     case 1:
+#         a = int(input("Masukkan angka 1 : "))
+#         b = int(input("Masukkan angka 2 : "))
+#         c = a + b 
+#         print (f"hasil penjumlahan angka 1 dan angka 2 adalah {c}")
+#     case 2:
+#         a = int(input("Masukkan angka 1 : "))
+#         b = int(input("Masukkan angka 2 : "))
+#         c = a - b 
+#         print (f"hasil pengurangan angka 1 dan angka 2 adalah {c}")
+#     case 3:
+#         a = int(input("Masukkan angka 1 : "))
+#         b = int(input("Masukkan angka 2 : "))
+#         c = a * b 
+#         print (f"hasil perkalian angka 1 dan angka 2 adalah {c}")
+#     case 4:
+#         a = int(input("Masukkan angka 1 : "))
+#         b = int(input("Masukkan angka 2 : "))
+#         c = a / b 
+#         print (f"hasil pembagian angka 1 dan angka 2 adalah {c}")
+
+# abis pertemuan coba selesaikan studi kasus dan
+# # print("nyari buku berapa?dan harga?")
+# # barang = int(input("jumlah barang:"))
+# # harga = int(input("harganya rek:"))
+# # if barang <= 5:
+# #    print()
+
+# masi error,pulang coba baikin dan
+# print("nilai brp bro?:")
+#  nilai=int(input("masukkan nilai bro:"))
+#  if nilai >0:
+#      if 75<nilai>=70:
+#         print("keren b-")
+#      if nilai>100:
+#         print("yailah ketingian")
+#      if 100<=nilai>=90:
+#         print("geloooo a+")
+#      if 90<nilai>=80:
+#         print("gokil a-")
+#      if 80<nilai>=75:
+#         print("gudjob b+")
+#      if 75<nilai>=70:
+#         print("keren b-")
+#      else:
+#         print("wahhhhhhhhhh")
+
+# y = int(input("angka:"))
+# if y >= 10 or y <= 20:
+#       print("mashok")
+# else: 
+#     print("g mashok")
